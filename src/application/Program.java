4147982cf7a532b6452f73e35e12d955b1d67c64
Paulo Gabriel <pgj1085@gmail.com>
2020-04-05 18:54:18 -0300

    Adicionando DaoFactory e SellerDaoJDBC

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,8 +2,9 @@ package application;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.util.Date;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -15,6 +16,9 @@ public class Program {
 		
 		
 		Seller obj = new Seller(1, "Paulo", "paulo@gmail.com", sdf.parse("10/08/1999"), 2750.0, new Department(1, "Computer"));
+		
+		
+		SellerDao sellerDao = DaoFactory.createSellerDao();
 		
 		System.out.println(obj);
 
