193d6ae24b99ef04faad44eda989883d4244002e
Paulo Gabriel <pgj1085@gmail.com>
2020-04-05 18:39:53 -0300

    Criando classe Seller

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,13 +1,20 @@
 package application;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws ParseException {
+		
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 		
 		
-		Department obj = new Department(1, "Books");
+		Seller obj = new Seller(1, "Paulo", "paulo@gmail.com", sdf.parse("10/08/1999"), 2750.0, new Department(1, "Computer"));
 		
 		System.out.println(obj);
 
