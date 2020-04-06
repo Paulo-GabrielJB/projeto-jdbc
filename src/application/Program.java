55843d6248da9dfa83570642fc1e26a20a6b4974
Paulo Gabriel <pgj1085@gmail.com>
2020-04-06 19:29:12 -0300

    Implementando o find by id

--- src/application/Program.java
+++ src/application/Program.java
@@ -12,15 +12,11 @@ public class Program {
 
 	public static void main(String[] args) throws ParseException {
 		
-		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
-		
-		
-		Seller obj = new Seller(1, "Paulo", "paulo@gmail.com", sdf.parse("10/08/1999"), 2750.0, new Department(1, "Computer"));
-		
-		
 		SellerDao sellerDao = DaoFactory.createSellerDao();
 		
-		System.out.println(obj);
+		Seller seller = sellerDao.findById(3);
+		
+		System.out.println(seller);
 
 	}
 
