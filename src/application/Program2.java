dadd0a6dd9cc040260c229eac8b8dd5551f9c5ff
Paulo Gabriel <pgj1085@gmail.com>
2020-04-07 10:54:43 -0300

    Implementando findAll department

---
+++ src/application/Program2.java
@@ -0,0 +1,24 @@
+package application;
+
+import java.util.List;
+
+import model.dao.DaoFactory;
+import model.dao.DepartmentDao;
+import model.entities.Department;
+
+public class Program2 {
+	
+	public static void main(String[] args) {
+		
+		DepartmentDao depDao = DaoFactory.createDepartmentDao();
+		
+		System.out.println("=== TEST 1: department findAll");
+		
+		List<Department> deps = depDao.findAll();
+		
+		for(Department d: deps)
+			System.out.println(d);
+		
+	}
+
+}
