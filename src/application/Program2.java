b07399b9c06dafcad0e09bce2175676636ab0481
Paulo Gabriel <pgj1085@gmail.com>
2020-04-07 11:12:06 -0300

    Implementando insert department

--- src/application/Program2.java
+++ src/application/Program2.java
@@ -20,9 +20,22 @@ public class Program2 {
 			System.out.println(d);
 		
 		
-		System.out.println("\n=== TEST 2: department findById");
+		System.out.println("\n=== TEST 2: department findById ===");
 		Department dep = depDao.findById(2);
 		System.out.println(dep);
+		
+		System.out.println("\n=== TEST 3: department deleteById ===");
+		
+		depDao.deleteById(5);
+		System.out.println("Delete completed");
+		
+		System.out.println("\n=== TEST 4: department insert ===");
+		
+		dep = new Department(null, "Cosmetics");
+		
+		depDao.insert(dep);
+		
+		System.out.println("Insert completed! Id department = " + dep.getId());
 		
 	}
 
