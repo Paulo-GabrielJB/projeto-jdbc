9b47adbf789febef0986f6c072d548e03a1088bb
Paulo Gabriel <pgj1085@gmail.com>
2020-04-07 10:42:19 -0300

    Implementando o delete by id

--- src/application/Program.java
+++ src/application/Program.java
@@ -3,6 +3,7 @@ package application;
 import java.text.ParseException;
 import java.util.Date;
 import java.util.List;
+import java.util.Scanner;
 
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
@@ -12,6 +13,8 @@ import model.entities.Seller;
 public class Program {
 
 	public static void main(String[] args) throws ParseException {
+		
+		Scanner sc = new Scanner(System.in);
 		
 		SellerDao sellerDao = DaoFactory.createSellerDao();
 		
@@ -39,11 +42,11 @@ public class Program {
 		
 		System.out.println("\n=== TEST 4: seller insert ===");
 		
-		//Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.0, dep);
+		Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.0, dep);
 		
-		//sellerDao.insert(newSeller);
+		sellerDao.insert(newSeller);
 		
-		//System.out.println("Inserted! New id = " + newSeller.getId());
+		System.out.println("Inserted! New id = " + newSeller.getId());
 		
 		System.out.println("\n=== TEST 5: seller update");
 		
@@ -55,6 +58,17 @@ public class Program {
 		
 		System.out.println("Update Completed");
 		
+		System.out.println("\n=== TEST 6: seller delete ===");
+		
+		System.out.println("Enter id for delete test: ");
+		
+		int id = sc.nextInt();
+		
+		sellerDao.deleteById(id);
+		
+		System.out.println("Delete completed");
+		
+		sc.close();
 		
 	}
 
